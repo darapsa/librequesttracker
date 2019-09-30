eaec859e3b77f02b5f7bd5e4f2b3d6c0cdbe9bfc
ꦌ ꦫꦶ ꦏ꧀ꦦꦿ ꦧ ꦮ ꦑ ꦩ ꦭ꧀ <erik@darapsa.co.id>
2019-09-30 11:34:20 +0800

    Ticket history structure
    and its related enumerations

--- rtclient/ticket.h
+++ rtclient/ticket.h
@@ -11,6 +11,37 @@ struct rtclient_ticket_list {
 	struct rtclient_ticket *tickets[];
 };
 
+enum rtclient_ticket_history_type {
+	RTCLIENT_TICKET_HISTORY_TYPE_CREATE
+	, RTCLIENT_TICKET_HISTORY_TYPE_EMAILRECORD
+	, RTCLIENT_TICKET_HISTORY_TYPE_SET
+	, RTCLIENT_TICKET_HISTORY_TYPE_SETWATCHER
+	, RTCLIENT_TICKET_HISTORY_TYPE_STATUS
+};
+
+enum rtclient_ticket_history_field {
+	RTCLIENT_TICKET_HISTORY_FIELD_NONE
+	, RTCLIENT_TICKET_HISTORY_FIELD_PRIORITY
+	, RTCLIENT_TICKET_HISTORY_FIELD_STATUS
+	, RTCLIENT_TICKET_HISTORY_FIELD_OWNER
+};
+
+struct rtclient_ticket_history {
+	unsigned int id;
+	unsigned int ticket;
+	unsigned int time_taken;
+	enum rtclient_ticket_history_type type;
+	enum rtclient_ticket_history_field field;
+	const char *old_value;
+	const char *new_value;
+	const char *data;
+	const char *description;
+	const char *content;
+	const char *creator;
+	struct tm *created;
+	const char *attachments;
+};
+
 #ifdef __cplusplus
 extern "C" {
 #endif
